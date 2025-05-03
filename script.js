e0f5006ce335b4987f28b323aa776a21fd172cd2
rohan37kumar <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>
2025-05-03 20:15:08 +0530

    added base game directory

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
     manifestUrl: 'https://olive-fashionable-mule-815.mypinata.cloud/ipfs/bafkreidzbh5ffgw5btdhfchx5qy3gkjxl3vexjpaww2yxgf2lvjvnhrn6m',
 });
-
+/*
 const connect = async()=>{
     await tonConnectUI.openModal();
+*/
+
+window.connectWallet = async () => {
+    try {
+        await tonConnectUI.openModal();
+        if (window.unityInstance) {
+            window.unityInstance.SendMessage('WalletManager', 'OnWalletConnected', 'success');
+        }
+    } catch (error) {
+        if (window.unityInstance) {
+            window.unityInstance.SendMessage('WalletManager', 'OnWalletConnected', 'error');
+        }
+    }
 }
