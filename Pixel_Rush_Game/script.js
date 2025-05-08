a7eaf0390d0fb849c658e2f00f20431e30f220a4
sh4d0wy <sakshambhugra8@gmail.com>
2025-05-08 07:37:41 +0530

    chore: Changed Manifest.json for pixel rush

--- Pixel_Rush_Game/script.js
+++ Pixel_Rush_Game/script.js
@@ -1,5 +1,5 @@
 const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
-    manifestUrl: 'https://olive-fashionable-mule-815.mypinata.cloud/ipfs/bafkreidzbh5ffgw5btdhfchx5qy3gkjxl3vexjpaww2yxgf2lvjvnhrn6m',
+    manifestUrl: 'https://olive-fashionable-mule-815.mypinata.cloud/ipfs/bafkreien4roprzwd3ov6wss2jjbnwkolglzt6ao3in52jmrjyvw4isfzva',
 });
 /*
 const connect = async()=>{
