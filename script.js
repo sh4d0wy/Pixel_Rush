13e7c348dbe0692a96e7c1a1372db812a221b304
rohan37kumar <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>
2025-05-09 22:09:29 +0530

    modify: wallet management feature

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
     manifestUrl: 'https://olive-fashionable-mule-815.mypinata.cloud/ipfs/bafkreidzbh5ffgw5btdhfchx5qy3gkjxl3vexjpaww2yxgf2lvjvnhrn6m',
 });
-/*
-const connect = async()=>{
-    await tonConnectUI.openModal();
-*/
 
 const connectWallet = async () => {
     try {
         await tonConnectUI.openModal();
         if (window.unityInstance) {
             window.unityInstance.SendMessage('WalletManager', 'OnWalletConnected', 'success');
+            getAccountData();
         }
     } catch (error) {
         if (window.unityInstance) {
